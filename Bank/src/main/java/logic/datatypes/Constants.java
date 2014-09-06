a9d3926dacf2c4c9ac299cbc5d065463eea536d7
leokraken <joleocl@gmail.com>
2014-09-06 14:30:15 -0300

    constantes

--- Bank/src/main/java/logic/datatypes/Constants.java
+++ Bank/src/main/java/logic/datatypes/Constants.java
@@ -5,8 +5,8 @@ import org.apache.activemq.ActiveMQConnection;
 public class Constants {
 	public static String url= ActiveMQConnection.DEFAULT_BROKER_URL;
 	public static String queuename= "TEST.queue";
-	public static String[] transaction_type = {"Compra","depósito","consulta de saldo","INVALID"};
-	public static String[] card_type = {"Débido","crédito","prepaga","INVALID"};
+	public static String[] transaction_type = {"compra","deposito","consulta de saldo","INVALID"};
+	public static String[] card_type = {"debido","credito","prepaga","INVALID"};
 	public static String[] money_code = {"UYU","USD","INVALID"};
 	public static String[] device_type= {"POS","WEB","ATM","INVALID"};
 	public static String[] dates= {"010114","010614","010814","0106140"};	//last invalid
